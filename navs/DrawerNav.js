3f67b2ea689ef380481772ff8621616469bca22d
Alex A <alex.andsten@gmail.com>
2023-08-30 22:28:56 +0200

    meny fix

--- navs/DrawerNav.js
+++ navs/DrawerNav.js
@@ -1,12 +1,19 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Profile } from '../screens/main/Profile';
+import { Chat } from '../screens/main/Chat';
+import TabNav from './TabNav';
 
 
 const Drawer = createDrawerNavigator();
 
 export default function DrawerNav() {
   return (
+    
     <Drawer.Navigator>
-     
+      
+      <Drawer.Screen name="Chat" component={Chat} />
+           <Drawer.Screen name="Profil" component={TabNav} />
+      
     </Drawer.Navigator>
   );
 }
